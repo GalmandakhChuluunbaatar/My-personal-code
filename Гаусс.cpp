3c56be97f7abac428351177be1bd73409fa04bb2
LaMeru <137993190+LaMeru@users.noreply.github.com>
2023-12-23 18:46:44 +0300

    Update Гаусс.cpp

--- Гаусс.cpp
+++ Гаусс.cpp
@@ -16,9 +16,9 @@ void VivodMatrix(int M, int N, double** matrix, int NumberOfShag, int ShagOfShag
 
 //Тут замена строки с нулем, на строку без нуля, шаг 1 в лекциях
 void ZamenaStroki(int M, int N, double** matrix, int number,int NumberOfShag,int ShagOfShag) {
-	if (matrix[number][number] == 0) {                                                                       //Выполняем когда элемент на диагонали равен 0
+	if (matrix[number][number] == 0) {                                                                                   //Выполняем когда элемент на диагонали равен 0
 		for (int i = number + 1; i < M; i++) {
-			if (matrix[i][number] != 0) {                                                                    //Ищем неравный
+			if (matrix[i][number] != 0) {                                                                        //Ищем неравный
 				double* array = new double[N]; for (int k = 0; k < N; k++) { array[k] = matrix[number][k]; } //Вписываем ненулевую строку в сторонний массив
 				for (int k = 0; k < N; k++) { matrix[number][k] = matrix[i][k]; }                            //Меняем первую на строку с ненулем
 				for (int k = 0; k < N; k++) { matrix[i][k] = array[k]; }                                     //Меняем строку с ненулем на первую
@@ -32,12 +32,12 @@ void ZamenaStroki(int M, int N, double** matrix, int number,int NumberOfShag,int
 //Обнуление вниз, это шаг 2
 void Obnulaem(int M, int N, double** matrix, int number, int  NumberOfShag, int ShagOfShag) {
 	for (int i = number + 1; i < M; i++) {
-		if (matrix[i][number] != 0) {                                                                         //Ищу ненулевые значения в столбце
-			double constanta = (matrix[i][number] / matrix[number][number]);                                  //Нахожу константу
+		if (matrix[i][number] != 0) {                                                      //Ищу ненулевые значения в столбце
+			double constanta = (matrix[i][number] / matrix[number][number]);           //Нахожу константу
 			for (int k = 0; k < N; k++) { 
-				matrix[i][k] = matrix[i][k] - matrix[number][k] * constanta;                                  //Отнимаю от строки с ненулевым значением нашу "диагональную" строку, умноженную на константу
+				matrix[i][k] = matrix[i][k] - matrix[number][k] * constanta;       //Отнимаю от строки с ненулевым значением нашу "диагональную" строку, умноженную на константу
 			}
-			VivodMatrix(M, N, matrix, NumberOfShag, ShagOfShag);                                              //Вызываю вывод
+			VivodMatrix(M, N, matrix, NumberOfShag, ShagOfShag);                       //Вызываю вывод
 		}
 	}
 }
@@ -76,9 +76,9 @@ int main()
 	
 	//Вывожу красивую систему
 	std::cout << "\nСистема: \n";
-	for (int i = M-1; i > -1; i--) {                                     //Отсчет с конца, так как внизу уже готовое значение переменной
+	for (int i = M-1; i > -1; i--) {                                                 //Отсчет с конца, так как внизу уже готовое значение переменной
 		for (int k = 0; k < N - 1; k++) {
-			if (matrix[i][k] != 0) {                                     //Если число не ноль
+			if (matrix[i][k] != 0) {                                         //Если число не ноль
 				std::cout << matrix[i][k] << char(97+k);                 //Вывожу коэффицент, умноженный на букву
 				if (k != N - 2) {                                        //Если не последнее число, пишу +
 					std::cout << " + ";
@@ -104,16 +104,16 @@ int main()
 	}
 
 	//Решаю систему
-	if (nul == false) {                                                                //Если есть нулевая строка, то систему оставляю в виде уравнения и не решаю
+	if (nul == false) {                                                                    //Если есть нулевая строка, то систему оставляю в виде уравнения и не решаю
 		int* array = new int[N - 1]; for (int i = 0; i < N - 1; i++) { array[i] = 0; } //Массив полученных переменных
 		std::cout << "\nРешаем: \n";
 		for (int i = M - 1; i > -1; i--) {                                             //Начиная с конца
 			double sum = 0;
 			for (int k = N - 2; k > -1; k--) {
-				if (k == i) break;                                                     //Складываю ДО "диагонального" элемента
-				sum += matrix[i][k] * array[k];                                        //Сумму формирую из значений переменных, умноженных на их коэффиценты
+				if (k == i) break;                                             //Складываю ДО "диагонального" элемента
+				sum += matrix[i][k] * array[k];                                //Сумму формирую из значений переменных, умноженных на их коэффиценты
 			}
-			array[i] = (matrix[i][N - 1] - sum) / matrix[i][i];                        //От числа после равно отнимаю сумма и делю на коэффицент
+			array[i] = (matrix[i][N - 1] - sum) / matrix[i][i];                    //От числа после равно отнимаю сумма и делю на коэффицент
 			std::cout << char(97 + M - i) << " = " << array[i] << "\n";
 		}
 	}
